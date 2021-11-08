1e23d1a098f30b7383513f258f913e8c7dde5aa3
mrvyilmaz <myilmaz@ktun.edu.tr>
2021-11-08 09:57:55 +0300

    ilk proje yüklendi

---
+++ BikeStoresProject/BikeStoresProject/Controllers/HomeController.cs
@@ -0,0 +1,117 @@
+﻿using BikeStoresProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BikeStoresProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            BikeStoresEntities ent = new BikeStoresEntities();
+            List<customers> model = new List<customers>();
+
+            try
+            {
+                model = ent.customers.OrderBy(q => q.first_name).ThenBy(q => q.last_name).ToList();
+
+                //select * from sales.customers where city = 'New York'
+                //model = ent.customers.Where(q => q.city.Equals("New York")).ToList();
+
+                //select * from sales.customers where first_name like 'Ab%' 
+                //model = ent.customers.Where(q => q.first_name.Substring(0, 2).Equals("Ab")).ToList();
+
+            }
+            catch (Exception) { throw; }
+
+            return View(model);
+        }
+
+        public ActionResult CustomerAdd(int? id = 0)
+        {
+            BikeStoresEntities ent = new BikeStoresEntities();
+            customers model = new customers();
+
+            try
+            {
+                if (id > 0)
+                {
+                    //Update
+                    model = ent.customers.Find(id);
+
+                }
+            }
+            catch (Exception) { throw; }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult CustomerAdd(customers model)
+        {
+            BikeStoresEntities ent = new BikeStoresEntities();
+            try
+            {
+                if (model.customer_id > 0)
+                {   //update
+
+                    customers customer = ent.customers.Find(model.customer_id);
+
+                    customer.first_name = model.first_name;
+                    customer.last_name = model.last_name;
+                    customer.phone = model.phone;
+                    customer.email = model.email;
+                    customer.street = model.street;
+                    customer.city = model.city;
+                    customer.state = model.state;
+                    customer.zip_code = model.zip_code;
+
+                    ent.SaveChanges();
+
+                }
+                else
+                {   //insert
+
+                    ent.customers.Add(model);
+                    ent.SaveChanges();
+                }
+            }
+            catch (Exception) { throw; }
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult CustomerDelete(int? id = 0)
+        {
+            BikeStoresEntities ent = new BikeStoresEntities();
+            customers model = new customers();
+            try
+            {
+                model = ent.customers.Find(id);
+                ent.customers.Remove(model);
+                ent.SaveChanges();
+
+            }
+            catch (Exception) { throw; }
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ BikeStoresProject/BikeStoresProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BikeStoresProject.MvcApplication" Language="C#" %>
