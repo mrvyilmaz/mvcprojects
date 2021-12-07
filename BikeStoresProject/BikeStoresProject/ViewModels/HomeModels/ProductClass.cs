6ce57ab376cbbd5e6effb4e261bf054f3841ff80
mrvyilmaz <myilmaz@ktun.edu.tr>
2021-12-07 10:38:02 +0300

    Ikinci Commit

---
+++ BikeStoresProject/BikeStoresProject/ViewModels/HomeModels/ProductClass.cs
@@ -0,0 +1,22 @@
+﻿using BikeStoresProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BikeStoresProject.ViewModels.HomeModels
+{
+    public class ProductClass
+    {
+        public ProductClass()
+        {
+            product = new products();
+            brandList = new List<brands>();
+            categoryList = new List<categories>();
+        }
+
+        public products product { get; set; }
+        public List<brands> brandList { get; set; }
+        public List<categories> categoryList { get; set; }
+    }
+}
