6939cd30ac94930cfe1f12d9a1ab9ab2baa5b834
mrvyilmaz <myilmaz@ktun.edu.tr>
2021-12-29 14:19:45 +0300

    Son Commit

--- BikeStoresProject/BikeStoresProject/Models/Model1.Designer.cs
+++ BikeStoresProject/BikeStoresProject/Models/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\KTUN\source\repos\BikeStoresProject\BikeStoresProject\Models\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\KTUN\Desktop\VT2_MVC\BikeStoresProject\BikeStoresProject\Models\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
